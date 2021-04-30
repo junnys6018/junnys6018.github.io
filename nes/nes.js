0f2828b48f18f8989e4f6df74237de9fa9e47548
junnys6018 <junkanglim1@gmail.com>
2021-04-30 16:01:11 +0800

    Made ui more responsive

--- nes/nes.js
+++ nes/nes.js
@@ -2,19 +2,21 @@ importScripts("emu.js", "message.js");
 
 let api;
 let ready = false;
-
 let keys = new Uint8Array(8);
+
 keys.fill(0);
 
+// Right now we are making 2 copies of ImageData and Float32Array each frame, once to copy it locally
+// in the worker thread, and another time to copy it into the parent thread. This is obviously stupid
+// TODO: make only one copy of each buffer per frame
 function Frame() {
-	let t1 = performance.now();
-
 	api.SetKeys(keys);
 	api.EmulateFrame();
 	
 	let pixelPtr = api.GetFrameBuffer();
 	let pixelView = new Uint8ClampedArray(Module.HEAPU8.buffer, pixelPtr, 240 * 256 * 4);
 	let pixelData = new ImageData(pixelView, 256, 240);
+	postMessage({type: MESSAGE_TYPE.SEND_PIXELS, pixels: pixelData});
 
 	let numSamples = api.FlushAudioSamples();
 	let audioBufPtr = api.GetAudioBuffer();
@@ -22,14 +24,9 @@ function Frame() {
 		let sampleView = new Float32Array(Module.HEAPF32.buffer, audioBufPtr, numSamples); // view of wasm memory
 		postMessage({ type: MESSAGE_TYPE.SEND_AUDIO, audio: new Float32Array(sampleView) }); // we pass a copy of the view
 	}
-
-	let t2 = performance.now();
-	totalTime += t2 - t1;
-	numFrames++;
-	postMessage({type: MESSAGE_TYPE.SEND_PIXELS, pixels: pixelData});
 }
 
-onmessage = function (e) {
+onmessage = (e) => {
 	if (ready) {
 		switch (e.data.type) {
 		case MESSAGE_TYPE.KEY_UP:
@@ -40,28 +37,10 @@ onmessage = function (e) {
 			break;
 		case MESSAGE_TYPE.LOAD_ROM:
 			api.LoadRom(e.data.rom);
-				break;
-		case MESSAGE_TYPE.PRINT_TIME:
-			printTime();
-			break;
-		case MESSAGE_TYPE.RESET_TIME:
-			resetTime();
 			break;
 		}
 	}
 };
-
-let totalTime = 0;
-let numFrames = 0;
-
-function printTime() {
-	console.log(`${numFrames} frames took ${totalTime}ms (avg: ${totalTime / numFrames})`);
-}
-
-function resetTime() {
-	totalTime = 0;
-	numFrames = 0;
-}
 
 Module.onRuntimeInitialized = async _ => {
 	api = Object.freeze({
@@ -79,13 +58,3 @@ Module.onRuntimeInitialized = async _ => {
 
 	setInterval(Frame, 16);
 };
-
-function Benchmark() {
-	let frames = 20000;
-	let t1 = performance.now();
-	for (let i = 0; i < frames; i++) {
-		api.EmulateFrame();
-	}
-	let t2 = performance.now();
-	console.log(`emulated ${frames} frames in ${t2 - t1}ms (avg: ${(t2 - t1) / frames}ms)`);
-}
